80b6104e3410e5a6e1893895165d85107907b7d6
Joel Cressy <joel@jtcressy.net>
2017-04-12 14:51:35 -0600

    updated js code so that it actually works

--- testcode.js
+++ testcode.js
@@ -1,17 +1,33 @@
- var x = Math.floor((Math.random() * 1420) + 1);
-
-
 $.ajax({
-    url: "http://xkcd.com/"+ x +"/info.0.json",
+    url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
     dataType: "json",
-    success: function(data) {
-        $("#xkcdcontent").append(
-            $("<h1 style=text-align:center;/>").text(data.title),
-            $("<img align=middle/>").attr({
-                src: data.img,
-                title: data.alt,
-                alt: data.title
-            })
-        );
-    }   
-});
+    jsonpCallback: "xkcddata",
+    success: function (response) {
+        var num = response.num;
+        var x = Math.floor((Math.random() * response.num) + 1);
+        ajax2(x);
+    }
+})
+function ajax2(x) {
+    $.ajax({
+        url: "http://dynamic.xkcd.com/api-0/jsonp/comic/" + x + "?callback=?",
+        dataType: "json",
+        jsonpCallback: "xkcddata",
+        success: function (data) {
+            $("#xkcdcontent").append(
+                $("<h1 style=text-align:center;/>").text(data.title),
+                $("<img align=middle/>").attr({
+                    src: data.img,
+                    title: data.alt,
+                    alt: data.title
+                }),
+                $("<h3 style=text-align:center;/>").text(data.alt),
+                $("<p style=text-align:center;/>").text("link to comic: https://www.xkcd.com/" + data.num + "/")
+            );
+        }
+    });
+}
+
+setTimeout(function() {
+  location.reload();
+}, 120000);
